edb34fec96333706585d91eea64e1d8d4118b8ab
El Ghali ASRI <drghaliasri@gmail.com>
2026-08-31 13:11:41 -0400

    testing favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,11 +18,16 @@ const amiri = Amiri({
 });
 
 export const metadata: Metadata = {
-  title: "البيان | مجلة علمية محكّمة",
-  description:
-    "مجلة علمية عربية للنشر المفتوح والبحث الخاضع للتحكيم الأقران في العلوم والهندسة والإنسانيات.",
-    icons: {
-    icon: "/favicon.ico",
+  title: "مجلة البيان",
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        type: "image/x-icon",
+      },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
   },
 };
 
